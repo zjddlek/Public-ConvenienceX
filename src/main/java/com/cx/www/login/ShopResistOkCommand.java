f0f68c8b0ea094734967fd5be3acc2f6c6254e07
LEC <leeec1211@naver.com>
2023-12-07 16:43:06 +0900

    점포코드 및 세션값저장

--- src/main/java/com/cx/www/login/ShopResistOkCommand.java
+++ src/main/java/com/cx/www/login/ShopResistOkCommand.java
@@ -2,6 +2,7 @@ package com.cx.www.login;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.cx.www.action.Action;
 import com.cx.www.dao.ShopDAO;
@@ -15,6 +16,7 @@ public class ShopResistOkCommand implements Action{
 		String sno = req.getParameter("postcode");
 		String sloc = req.getParameter("roadaddrs");
 		String bno = req.getParameter("businessNo");
+		String email = req.getParameter("email") + "@" + req.getParameter("emailAddrs");
 		
 		ShopDAO dao = new ShopDAO();
 		ShopVO svo = new ShopVO(sno, sname, sloc, bno);
@@ -22,9 +24,14 @@ public class ShopResistOkCommand implements Action{
 		
 		dao.close();
 		
-		System.out.println("등록하러옴");
+		HttpSession session = req.getSession();
+		session.setAttribute("sno", sno);
+		session.setAttribute("email", email);
 		
-		return "login/login.jsp";
+		
+		//System.out.println("등록하러옴");
+		
+		return "emp/register.jsp";
 	}
 
 }
